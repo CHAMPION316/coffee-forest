7a9747c1dad28d948e04d0322328c1344dfada40
C316 <royer.seguracalderon@gmail.com>
2022-05-29 15:49:38 +0000

    Fix name of variable

--- products/views.py
+++ products/views.py
@@ -10,7 +10,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    category = None
+    categories = None
 
     if request.GET:
         if 'category' in request.GET:
