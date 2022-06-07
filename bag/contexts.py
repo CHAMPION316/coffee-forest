273a6b822aac01f547412ffb19129426239a2415
C316 <royer.seguracalderon@gmail.com>
2022-06-07 21:23:58 +0000

    Add import Product

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,7 +1,7 @@
 from decimal import Decimal 
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from products.models import import Product
+from products.models import Product
 
 def bag_contents(request):
 
