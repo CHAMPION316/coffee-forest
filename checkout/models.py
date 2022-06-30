db99d91f80d807fe2c5f30eb7ce98565437689ed
C316 <royer.seguracalderon@gmail.com>
2022-06-30 17:28:56 +0000

    Include proper imports for models.py in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,10 @@
+import uuid
+
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Product
 
 
 class Order(models.Model):
