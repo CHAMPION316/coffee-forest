aa9aa331506a928f2bae1427c78f5b33546ca105
C316 <royer.seguracalderon@gmail.com>
2022-06-30 18:46:55 +0000

    Create fields for admin.py in checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date', 
+                        'delivery_cost', 'order_total', 
+                        'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name', 
+              'email', 'phone_number', 'country', 
+              'postcode', 'town_or_city', 'street_address', 
+              'county', 'delivery_cost', 'order_total', 'grand_total',)
