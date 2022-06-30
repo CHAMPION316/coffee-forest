27e6ffd973d7986c842b2ae54cf6bed37c5a96ee
C316 <royer.seguracalderon@gmail.com>
2022-06-30 17:25:41 +0000

    Create OrderLineItem model in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Order(models.Model):
-
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, editable=False)
     email = models.CharField(max_length=100, null=False, editable=False)
@@ -16,3 +15,11 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=Flase, blank=False, editable=False)
