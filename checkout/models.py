02d362debdad13a912bd6140e162a5a012fe6d70
C316 <royer.seguracalderon@gmail.com>
2022-06-30 19:43:42 +0000

    create OrderLineItem class for models.py in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -28,4 +28,4 @@ class OrderLineItem(models.Model):
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=Flase, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
