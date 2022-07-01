6cf2ea8242e3df7fe96d87e5775f32bdf7bbbb6c
C316 <royer.seguracalderon@gmail.com>
2022-07-01 17:31:12 +0000

    Include neccessary extensions to use crispy forms in settings.py

--- coffee_forest/settings.py
+++ coffee_forest/settings.py
@@ -63,6 +63,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'coffee_forest.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -79,6 +81,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
