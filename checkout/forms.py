9d108c3db1297b0d784bf8790fa44c051b37f62a
C316 <royer.seguracalderon@gmail.com>
2022-07-01 12:22:16 +0000

    Complete addition of forms, admin, and signals

--- checkout/forms.py
+++ checkout/forms.py
@@ -26,3 +26,13 @@ class OrderForm(forms.ModelForm)
             'street_address2': 'Street Address 2',
             'county': 'County',
         }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
