c52141071817450618a7db209054f08d947128ce
C316 <royer.seguracalderon@gmail.com>
2022-07-01 12:20:15 +0000

    Create __init__ for placeholders and classes

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,3 +8,21 @@ class OrderForm(forms.ModelForm)
         fields = ('full_name', 'email', 'phone_number',
                   'street_address', 'town_or_city',
                   'post_code', 'country', 'county',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County',
+        }
