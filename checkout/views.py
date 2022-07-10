a8ad557348c2cc8c169edd89b3f4d67af4a43862
C316 <royer.seguracalderon@gmail.com>
2022-07-10 10:27:10 +0000

    Added stripe key for payments

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LJmKuC7HW1slj1MR56rpzn6hn2pKpCl4w5R3xRkdwL2p44bCcBlooLBk5YwVqGGIOReGmRAwJfiywlkh7enx4Jz00ju7O4ayL',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
