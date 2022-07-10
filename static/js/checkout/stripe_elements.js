50a2f3da258b132556ae3cd1f25fb1da1fc987ae
C316 <royer.seguracalderon@gmail.com>
2022-07-10 10:27:47 +0000

    Created a static/js/ folder

---
+++ static/js/checkout/stripe_elements.js
@@ -0,0 +1,8 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text
