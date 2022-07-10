b25dd3afd8c595c950e1440d9bfdd78a44690c58
C316 <royer.seguracalderon@gmail.com>
2022-07-10 10:47:58 +0000

    Create logic for card fields to work

--- static/js/checkout/stripe_elements.js
+++ static/js/checkout/stripe_elements.js
@@ -5,4 +5,5 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text
+var stripe_public_key = $('#id_stripe_public_key').text.slice(1, -1);
+var client_secret = $('#id_client_secret').text.slice(1, -1);
