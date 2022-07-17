ec5b841dfaecc3a9287b2910d9bfd42ad344196f
C316 <royer.seguracalderon@gmail.com>
2022-07-17 11:49:25 +0000

    Update content for HttpResponse message

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -12,7 +12,7 @@ class StripeWH_Handler:
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_succeeded(self, event):
