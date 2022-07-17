eb6d92c078ee091451ef59e1205c0fcd9da7dfe7
C316 <royer.seguracalderon@gmail.com>
2022-07-17 14:35:06 +0000

    Create a request for cache_checkout_data

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -10,6 +11,11 @@ from bag.contexts import bag_contents
 import stripe
 
 
+@require_POST
+def cache_checkout_data(request):
+
+
+    
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
