b65e89bf441be939c57c90de6c9f218e5ea99f39
C316 <royer.seguracalderon@gmail.com>
2022-07-17 11:29:10 +0000

    function for handling payment using Stripe

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -14,3 +14,11 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
