82dd86c3c1e6d4edfbf264f95877909359f3b332
C316 <royer.seguracalderon@gmail.com>
2022-07-17 15:04:09 +0000

    Complete eventlistener incase of payment issues or success

--- static/js/checkout/stripe_elements.js
+++ static/js/checkout/stripe_elements.js
@@ -52,49 +52,64 @@ form.addEventListener('submit', function(ev) {
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-            billing_details: {
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // From using {% csrf_token %} in the form
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+    $.post(url, postData).done(function() {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $trim(form.phone_number.value),
+                    email: $trim(form.email.value),
+                    address: {
+                        address: $.trim(form.street_address.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $trim(form.country.value),
+                        state: $trim(form.county.value),
+                    }
+                }
+            },
+            shipping: {
                 name: $.trim(form.full_name.value),
                 phone: $trim(form.phone_number.value),
-                email: $trim(form.email.value),
                 address: {
                     address: $.trim(form.street_address.value),
                     city: $.trim(form.town_or_city.value),
                     country: $trim(form.country.value),
+                    postal_code: $trim(form.postcode.value),
                     state: $trim(form.county.value),
                 }
+            },
+        }).then(function(result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="bi bi-x-square"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({ 'disabled': false});
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
             }
-        },
-        shipping: {
-            name: $.trim(form.full_name.value),
-            phone: $trim(form.phone_number.value),
-            address: {
-                address: $.trim(form.street_address.value),
-                city: $.trim(form.town_or_city.value),
-                country: $trim(form.country.value),
-                postal_code: $trim(form.postcode.value),
-                state: $trim(form.county.value),
-            }
-        }
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                <i class="bi bi-x-square"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            $('#payment-form').fadeToggle(100);
-            $('#loading-overlay').fadeToggle(100);
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
-            }
-        }
-    });
+        });
+    }).fail(function () {
+        // Reload the page, the error will be in the django messages
+        location.reload();
+    })
 });
