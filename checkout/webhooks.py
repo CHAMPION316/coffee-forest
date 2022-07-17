044d9d2cf992d5eccb520fa5966b72c2b40a9319
C316 <royer.seguracalderon@gmail.com>
2022-07-17 11:57:36 +0000

    Add imports for webhooks.py to function

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,5 +1,14 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
+from checkout.webhook_handler import StripeWH_Handler
 
+import stripe
+
+@require_POST
+@csrf_exempt
 def webhook(request):
     """Listen for webhooks from Stripe"""
     # Setup
