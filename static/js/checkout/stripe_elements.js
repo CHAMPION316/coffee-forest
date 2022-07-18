e67530db1f4b4c27ba3838157e3db9d71306c2f4
C316 <royer.seguracalderon@gmail.com>
2022-07-18 15:27:54 +0000

    Forot to add $. infront of billing_details and changed a dettail name

--- static/js/checkout/stripe_elements.js
+++ static/js/checkout/stripe_elements.js
@@ -29,7 +29,7 @@ card.mount('#card-element');
 
 // The handling of realtime errors on the card element
 card.addEventListener('change', function (event) {
-    var errorDiv = document.getElementById('card-erros');
+    var errorDiv = document.getElementById('card-errors');
     if (event.error) {
         var html= `
             <span class="icon" role="alert">
@@ -68,25 +68,25 @@ form.addEventListener('submit', function(ev) {
                 card: card,
                 billing_details: {
                     name: $.trim(form.full_name.value),
-                    phone: $trim(form.phone_number.value),
-                    email: $trim(form.email.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
                     address: {
-                        address: $.trim(form.street_address.value),
+                        line1: $.trim(form.street_address.value),
                         city: $.trim(form.town_or_city.value),
-                        country: $trim(form.country.value),
-                        state: $trim(form.county.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
                     }
                 }
             },
             shipping: {
                 name: $.trim(form.full_name.value),
-                phone: $trim(form.phone_number.value),
+                phone: $.trim(form.phone_number.value),
                 address: {
-                    address: $.trim(form.street_address.value),
+                    line1: $.trim(form.street_address.value),
                     city: $.trim(form.town_or_city.value),
-                    country: $trim(form.country.value),
-                    postal_code: $trim(form.postcode.value),
-                    state: $trim(form.county.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
                 }
             },
         }).then(function(result) {
