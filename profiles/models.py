fcac21c387d50ead469fd48c6f27c8b4f244e47d
C316 <royer.seguracalderon@gmail.com>
2022-07-21 16:12:07 +0000

    Creat models for profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and orders
+    """
+    default_phone_number = models.CharField(max_length=10, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_postcode = models.CharField(max_length=10, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=32, null=True, blank=True)
+    default_street_address = models.CharField(max_length=100, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
