8d330f35dcfa82d0d77cc9d4f1e3bb5de169c9b5
C316 <royer.seguracalderon@gmail.com>
2022-07-21 13:52:14 +0000

    Fix class for add_product in views.py

--- products/views.py
+++ products/views.py
@@ -69,19 +69,10 @@ def product_detail(request, product_id):
     
     return render(request, 'products/product_detail.html', context)
 
+
 def add_product(request):
     """ Add a product to the store """
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
-        else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
-    else:
-        form = ProductForm()
-        
+    form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
