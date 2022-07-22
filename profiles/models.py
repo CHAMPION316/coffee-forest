4531903495ad14b10764852bf6936d5272c060b7
C316 <royer.seguracalderon@gmail.com>
2022-07-22 21:40:49 +0000

    Rearranged the label orders

--- profiles/models.py
+++ profiles/models.py
@@ -14,11 +14,11 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=10, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=10, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=32, null=True, blank=True)
     default_street_address = models.CharField(max_length=100, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=32, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=10, null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
