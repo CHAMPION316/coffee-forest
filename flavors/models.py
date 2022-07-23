02850500e710a5c3fb357b9985915eae550f9eec
C316 <royer.seguracalderon@gmail.com>
2022-07-23 15:17:19 +0000

    Include def __str__(self): for models.py in flavors app

--- flavors/models.py
+++ flavors/models.py
@@ -10,6 +10,14 @@ class Flavor(models.Model):
     class Meta:
         verbose_name_plural = 'Flavors'
 
-
     flavors_title = models.CharField(max_length=30, null="True", blank="True")
     flavors_description = models.TextField()
+
+    def __str__(self):
+        return self.flavors_title
+
+    def __str__(self):
+        return self.flavors_description
+
+
+
