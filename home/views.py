17335a5ab192b801603bfec229528a8e21afc628
C316 <royer.seguracalderon@gmail.com>
2022-07-26 11:42:12 +0000

    Fix views.py by adding a .get() method to render the needed object for the template

--- home/views.py
+++ home/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from.models import Flavor
+from django import template
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
-    flavors = Flavor.objects.filter().only('description_f')
+    flavors = Flavor.objects.order_by('description_f')[0:1].get()
     context = {
         'flavors': flavors
     }
     return render(request, 'home/index.html', context)
-
-
-# def index(request):
-#     """ A view to return the index page """
-    
-#     return render(request, 'home/index.html')
