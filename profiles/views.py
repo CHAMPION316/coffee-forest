2b68246230c3b0ca674fa38493efb878a59a8974
C316 <royer.seguracalderon@gmail.com>
2022-07-26 00:03:38 +0000

    Create a POST request for when profile form is updated

--- profiles/views.py
+++ profiles/views.py
@@ -8,6 +8,12 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
@@ -15,6 +21,7 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
