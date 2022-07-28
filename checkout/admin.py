0dc51b198b6c36ac032b92c5c15a29ce33de9c26
C316 <royer.seguracalderon@gmail.com>
2022-07-28 21:46:17 +0000

    insert 'user_profile' to fields for OrderAdmin class

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,10 +12,10 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date', 
                         'delivery_cost', 'order_total', 
-                        'grand_total', 'original_bag', 
+                        'grand_total', 'original_bag',
                         'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name', 
+    fields = ('order_number', 'user_profile', 'date', 'full_name', 
               'email', 'phone_number', 'country', 
               'postcode', 'town_or_city', 'street_address', 
               'county', 'delivery_cost', 'order_total', 
