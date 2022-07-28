d1f524aa1aa509d4099b3ce7b321c45933ea21a3
C316 <royer.seguracalderon@gmail.com>
2022-07-28 01:13:02 +0000

    Add space between from and .models

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from.models import Flavor, Title, History
+from .models import Flavor, Title, History
 from django import template
 
 from django.core.mail import send_mail
