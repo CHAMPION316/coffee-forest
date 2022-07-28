a88085e895120cd7ff65c526ab6582d96daa49a5
C316 <royer.seguracalderon@gmail.com>
2022-07-28 01:13:48 +0000

    Create global_context for class Hisotry in contexts.py

--- home/contexts.py
+++ home/contexts.py
@@ -1,9 +1,12 @@
 from django.shortcuts import get_object_or_404
-from .models import Title, Flavor
+from .models import Title, Flavor, History
 
 
 def global_context(request):
 
     return {
         'titles': get_object_or_404(Title, pk=1),
+        'flavors': get_object_or_404(Flavor),
+        'histories': get_object_or_404(History, pk=1),
+        'histories2': get_object_or_404(History, pk=2),
     }
