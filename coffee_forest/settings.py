dff56222b0d6d083d42bf40e5d057dd7365810a5
C316 <royer.seguracalderon@gmail.com>
2022-07-29 17:40:40 +0000

    Fix comment for email out of stripe

--- coffee_forest/settings.py
+++ coffee_forest/settings.py
@@ -187,6 +187,8 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+# Default Email
 DEFAULT_FROM_EMAIL = os.getenv('coffeeforest@example.com')
 
 # Default primary key field type
