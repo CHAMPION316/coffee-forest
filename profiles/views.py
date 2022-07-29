6c86141154635ef774c0c8bd11b4eec017c14f7b
C316 <royer.seguracalderon@gmail.com>
2022-07-29 01:34:31 +0000

    Fix identation issue on if else statement

--- profiles/views.py
+++ profiles/views.py
@@ -16,8 +16,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please review the form.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
