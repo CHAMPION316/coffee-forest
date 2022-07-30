831b83651343f2483fe3d48273fc1540385793b2
C316 <royer.seguracalderon@gmail.com>
2022-07-30 13:18:13 +0000

    Fix identation and whitespace

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,4 @@
-from decimal import Decimal 
+from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
@@ -21,17 +21,17 @@ def bag_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
-        else: 
+        else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price 
-                product_count += quantity  
+                total += quantity * product.price
+                product_count += quantity
                 bag_items.append({
-                'item_id': item_id,
-                'quantity': quantity,
-                'product': product,
-                'size': size,
-            })
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                    })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -39,7 +39,7 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
 
     context = {
