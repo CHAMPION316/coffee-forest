c8f516bbc8b3ca0ac1e7cb1a6a79dff1ee4d9dcd
C316 <royer.seguracalderon@gmail.com>
2022-09-22 14:51:19 +0000

    Fix pep8 validation on apps.py

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,7 +1,11 @@
+"""checkout config for checkout"""
 from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
+    """
+    checkout class default
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
 
