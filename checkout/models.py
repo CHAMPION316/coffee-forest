b13ad70213c3ae8a1ba4c0853e5726cefb86e92e
C316 <royer.seguracalderon@gmail.com>
2022-09-22 00:05:09 +0000

    Fix line too long issue

--- checkout/models.py
+++ checkout/models.py
@@ -50,7 +50,8 @@ class Order(models.Model):
         a new line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
