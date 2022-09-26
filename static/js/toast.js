03f7484ae6f7fd25259d9b961792774b63a7a589
C316 <royer.seguracalderon@gmail.com>
2022-09-26 12:16:53 +0000

    Comment out toast.js to check for eventlistener error

--- static/js/toast.js
+++ static/js/toast.js
@@ -1,8 +1,8 @@
-const toastBtnRef = document.querySelector("#toastbtn")
-toastBtnRef.addEventListener("click",()=>{
-    const toastElList = [].slice.call(document.querySelectorAll('.toast'))
-    const options = {delay: 3000}
-    toastElList.map(function(toastEl) {
-      return new bootstrap.Toast(toastEl, options)
-    })
-})
+// const toastBtnRef = document.querySelector("#toastbtn")
+// toastBtnRef.addEventListener("click",()=>{
+//     const toastElList = [].slice.call(document.querySelectorAll('.toast'))
+//     const options = {delay: 3000}
+//     toastElList.map(function(toastEl) {
+//       return new bootstrap.Toast(toastEl, options)
+//     })
+// })
