d729f657da398de868f8a9b9d0c13bb17569238d
C316 <royer.seguracalderon@gmail.com>
2022-09-27 09:06:43 +0000

    Delete toast.js file to check for error fix

--- static/js/toast.js
+++
@@ -1,8 +0,0 @@
-// const toastBtnRef = document.querySelector("#toastbtn")
-// toastBtnRef.addEventListener("click",()=>{
-//     const toastElList = [].slice.call(document.querySelectorAll('.toast'))
-//     const options = {delay: 3000}
-//     toastElList.map(function(toastEl) {
-//       return new bootstrap.Toast(toastEl, options)
-//     })
-// })
