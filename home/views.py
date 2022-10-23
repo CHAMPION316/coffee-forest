f4b60bcc308b6f372bb78ae5a1783765b57be2b7
C316 <royer.seguracalderon@gmail.com>
2022-10-23 01:04:43 +0000

    Changed the object type for flavors to all()

--- home/views.py
+++ home/views.py
@@ -15,7 +15,7 @@ def index(request):
     """ A view to return the index page """
 
 
-    flavors = Flavor.objects.order_by('description_f')[0:1].get()
+    flavors = Flavor.objects.all()
     titles = Title.objects.order_by('title_f')[0:1].get()
     histories = History.objects.order_by('history_f')[0:1].get()
     histories2 = History.objects.order_by('history_f')[1:2].get()
