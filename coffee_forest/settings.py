cbd1a201de29dd27a0b652bea0622b8ead738613
C316 <royer.seguracalderon@gmail.com>
2022-10-24 19:36:01 +0000

    Add os.environ key to settings.py

--- coffee_forest/settings.py
+++ coffee_forest/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['coffee-forest.herokuapp.com', 'localhost']
 
@@ -200,6 +200,7 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    USE_AWS = os.environ.get('USE_AWS')
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
