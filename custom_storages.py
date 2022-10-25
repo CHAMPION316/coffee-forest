493b9d8d205c74611d4fe22cd47d717f36f7ae46
C316 <royer.seguracalderon@gmail.com>
2022-10-25 02:55:21 +0000

    Add docstring for python validation

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,16 @@
+"""
+Imports and classes to upload
+staticfiles to AWS
+"""
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """ storage for static files to AWS """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """ storage for media files to AWS """
     location = settings.MEDIAFILES_LOCATION
