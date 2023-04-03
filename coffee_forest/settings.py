bbc30b2872a1d68527a6c322981ab804cf40573e
C316 <royer.seguracalderon@gmail.com>
2023-04-03 16:32:44 +0000

    Update environment variables for deployment on render

--- coffee_forest/settings.py
+++ coffee_forest/settings.py
@@ -30,8 +30,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['coffee-forest.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['coffee-forest.herokuapp.com', 'coffee-forest.onrender.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
